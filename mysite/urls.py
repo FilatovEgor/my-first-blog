1d490bc2ce1d12a6bb0635c8c2ac6b927a439c90
Filatov Egor <filatovegor@mail.ru>
2016-01-31 02:02:31 +0300

    Testing 2

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
-]
+)
